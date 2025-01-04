a846eb418a5318aea1cb27e791bbe863a16d8f08
Jovan Cvetkov <yo1.cvetkov@gmail.com>
2025-01-04 23:51:01 +0100

    Added express and intialized the server

---
+++ main.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+const bootstrap = async () => {
+  const app = express();
+
+  app.listen(process.env.PORT, () => {
+    console.log(`Server is listening on port http://localhost:${process.env.PORT}`);
+  });
+};
